efa4e0ba720570f009531e9b2a564bff1900e91a
RubenHernandez2022 <ben_arg@hotmail.com>
2023-04-17 01:51:55 -0300

    proy api

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const express= require(`express`);
-const app=express();
+const express=require('express');
+const {todoProd,agregarUnProducto} = require('./controllers/adminControllers');
+const cors =require('cors');
+const app= express();
 const puerto=4000;
-const {atodoProd,agregarunProd}=require("./controllers/adminControllers");
 
 app.use(express.json());
-
-app.get("./getatodoProd",atodoProd);
-
-
-app.post("./Administrador",agregarunProd);
+app.use(cors());// por si el navegador nos tira un error porque estamos queriendo consumir de un dominio a otro 
+app.use(express.urlencoded({extended:true}))//para poder recibir la info de un formulario en formato de objeto 
 
 
+app.get('/pedirProducto',todoProd);//peticion get que me traer a todos los articulos
+
+
+app.post('/agregarProducto',agregarUnProducto);
+//http://localhost:4001/addarticulo
 
 app.listen(puerto,()=>{
-    console.log(`conectado al puerto ${puerto}`)
-})
+    console.log(`Escuchando por el puerto  ${puerto}`)
+    
+});
+// pedir los articulos -> get
+// agregar articulos a la lista   -> post
--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,33 +1,45 @@
-const articulos=[{
-    tipoProd:"Materiales",
-    producto:"Cemento",
-    marca:"Loma Negra"
-}
-,{tipoProd:"Herramientas",
-  producto:"pala",
-  marca:"la mas"
+//la info va a venir de una Base de Datos
 
-}
-,{
-   tipoProd:"Materiales",
-   producto:"Cal",
-   marca:"Grobo"}
-]
+const Articulos=[
+  {
+     TipoProd:"Materiales",
+     Producto:"Cemento",
+     Marca:"Loma Negra"
+  }
+ ,{
+     TipoProd:"Herramientas",
+     Producto:"pala",
+     Marca:"la mas"
+  }
+ ,{
+    TipoProd:"Materiales",
+    Producto:"Cal",
+    Marca:"Grobo"
+  }
+ ]
+/*---------------*/
 
-const atodoProd=(req,res)=>{
-    let infoJson=JSON.stringify(articulos);//hasta q tga base de datos
-    console.log(infoJson);
-    res.send(infoJson);
+
+const todoProd=(req,res)=>{
+
+ // res.status(200) -> valor por defecto, le respondo un estado OK
+ //formato ? json
+ let infoJSON=JSON.stringify(Articulos)//alterando students para que se envie en formato JSON
+ console.log(infoJSON)
+ res.send(infoJSON)
+};
+
+const agregarUnProducto =(req,res)=>{
+
+  const {tipoProd,producto,marca}=req.body// de toda la info del formulario, solo me estoy guardando en una variable "nombre" , el valor del input de name "nombre"
+  console.log(tipoProd)
+  Articulos.push({TipoProd:tipoProd,Producto:producto,Marca:marca})
+  console.log(Articulos)
+//responder algo ..  salio todo ok -> rediriga a una peticion del front .
+// res.send(" nombre del estudiante recibido")
+  res.redirect("http://localhost:3000/")
+  //res.status(200).massege("sdhfkjshd") .send(mandarDatos) .redirect()
 };
 
 
-
-const agregarunProd=(req,res)=>{
-const {tipoProd}=req.body
-
-    console.log(tipoProd)
-    atodoProd.push({tipoProd:tipoProd})
-    res.redirect("http://localhost:3000/")
-};
-
-module.exports={atodoProd,agregarunProd};
+module.exports={todoProd,agregarUnProducto}
