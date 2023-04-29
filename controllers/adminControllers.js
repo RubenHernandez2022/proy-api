8b4a321b17fcc4d211077681092dedf7ea981e69
RubenHernandez2022 <ben_arg@hotmail.com>
2023-04-29 02:09:10 -0300

    proyapi

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -7,21 +7,24 @@ const Articulos=[
      Marca:"Loma Negra",
      Imagen:"./imagenes/cemento loma-negra.jpg"
   }
- ,{
-     TipoProd:"Herramientas",
-     Producto:"pala",
-     Marca:"la mas",
-     Imagen:"./imagenes/pala.jpg"
-  }
- ,{
-    TipoProd:"Materiales",
-    Producto:"Cal",
-    Marca:"Santa Barbara",
-    Imagen:"./imagenes/cal santa-barbara.jpg"
-  }
+//  ,{
+//      TipoProd:"Herramientas",
+//      Producto:"pala",
+//      Marca:"la mas",
+//      Imagen:"./imagenes/pala.jpg"
+//   }
+//  ,{
+//     TipoProd:"Materiales",
+//     Producto:"Cal",
+//     Marca:"Santa Barbara",
+//     Imagen:"./imagenes/cal santa-barbara.jpg"
+//   }
  ]
 /*---------------*/
-
+require('../database');
+ let mongoose= require('mongoose');
+ let ArticulosDB =require('../modelArticulos');
+ let ArticuloDB= mongoose.model('Artículo',ArticulosDB);
 
 const todoProd=(req,res)=>{
 
@@ -32,12 +35,26 @@ const todoProd=(req,res)=>{
  res.send(infoJSON)
 };
 
+
+
 const agregarUnProducto =(req,res)=>{
 
   const {tipoProducto,producto,marca}=req.body// de toda la info del formulario, input
  
   Articulos.push({TipoProd:tipoProducto,Producto:producto,Marca:marca})
   console.log(Articulos)
+  
+  let doc= new ArticuloDB({tipoProducto:String,Producto:String,Marca:String})
+
+ let unArticulo= new ArticuloDB(
+    {tipoProducto:tipoProducto,
+      Producto:producto,
+      Marca:marca
+    }
+ )
+ doc.collection.insertOne(unArticulo)
+.then((info)=>console.log(info))
+.catch(err=>console.log(err))
 
 // res.send(" nombre articulo recibido")
   res.redirect("http://localhost:3000/")
